0df71618307f34ab01e01182a260a36d6b757230
Matth-Ben <matthiasbenoit.pro@gmail.com>
2026-01-04 15:18:00 +0100

    feat: Add Electron preload script for window controls and auto-update, and include application icon.

--- electron/preload.js
+++ electron/preload.js
@@ -1,14 +1,30 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('electronAPI', {
+    // Window controls
     minimize: () => ipcRenderer.send('window-minimize'),
     maximize: () => ipcRenderer.send('window-maximize'),
     close: () => ipcRenderer.send('window-close'),
 
     // Auto-update APIs
-    onUpdateChecking: (callback) => ipcRenderer.on('update-checking', callback),
-    onUpdateAvailable: (callback) => ipcRenderer.on('update-available', (event, info) => callback(info)),
-    onDownloadProgress: (callback) => ipcRenderer.on('download-progress', (event, percent) => callback(percent)),
-    onUpdateDownloaded: (callback) => ipcRenderer.on('update-downloaded', (event, info) => callback(info)),
+    onUpdateChecking: (callback) => {
+        ipcRenderer.on('update-checking', callback)
+        return () => ipcRenderer.removeListener('update-checking', callback)
+    },
+    onUpdateAvailable: (callback) => {
+        const listener = (event, info) => callback(info)
+        ipcRenderer.on('update-available', listener)
+        return () => ipcRenderer.removeListener('update-available', listener)
+    },
+    onDownloadProgress: (callback) => {
+        const listener = (event, percent) => callback(percent)
+        ipcRenderer.on('download-progress', listener)
+        return () => ipcRenderer.removeListener('download-progress', listener)
+    },
+    onUpdateDownloaded: (callback) => {
+        const listener = (event, info) => callback(info)
+        ipcRenderer.on('update-downloaded', listener)
+        return () => ipcRenderer.removeListener('update-downloaded', listener)
+    },
     installUpdate: () => ipcRenderer.send('install-update'),
 })
